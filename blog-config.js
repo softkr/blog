7b85b19cc5eb80ead7902b1468d311ed2847d196
Elijah Roh <softkr@kakao.com>
2024-12-07 09:56:48 +0900

    first commit

---
+++ blog-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: "The SoftKR Log",
+  description:
+    "Exploring innovative solutions in software development, AI, and cutting-edge technologies.",
+  author: "John Roh",
+  siteUrl: "https://softkr.dev",
+  links: {
+    github: "https://github.com/softkr", // GitHub 계정 URL
+    // linkedIn: "https://linkedin.com/in/[YourLinkedInUsername]", // LinkedIn 프로필 URL
+    // facebook: "https://www.facebook.com/[YourFacebookUsername]", // Facebook 계정 URL
+    // twitter: "https://twitter.com/[YourTwitterUsername]", // Twitter 계정 URL
+    // x: "https://x.com/[YourTwitterUsername]", // X.com (Twitter 대체 도메인) URL
+    // instagram: "https://www.instagram.com/[YourInstagramUsername]", // Instagram 계정 URL
+    // medium: "https://medium.com/@[YourMediumUsername]", // Medium 계정 URL
+    // blogger: "https://[YourBloggerSubdomain].blogspot.com", // Blogger 블로그 URL
+    email: "mailto:softkr@kakao.com", // 이메일 주소
+    resume: "https://softkr.dev/resume", // 이력서 링크 (적절히 변경)
+    link: "https://softkr.dev", // 주 도메인 링크
+  },
+  useAbout: true,
+  // See https://giscus.app/
+  giscus: {
+    repo: "softkr/softkr.dev", // 본인의 GitHub 저장소
+    repoId: "MDEwOlJlcG9zaXRvcnkzNjk4NjMzNTg=", // 저장소 ID (API 또는 giscus 설정 시 제공)
+    category: "General", // GitHub Discussions 카테고리 이름
+    categoryId: "DIC_kwDOFguqvs4ChwGy", // 카테고리 ID
+    mapping: "pathname", // 페이지 경로 기반 댓글 연결
+    strict: "0", // 비엄격 모드
+    reactionsEnabled: "1", // 반응 활성화
+    inputPosition: "bottom", // 댓글 입력창 위치
+    lang: "ko", // 언어 설정 (한국어는 "ko")
+  },
+}
